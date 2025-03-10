6ee871fd98fe95eb0dafd7e3cb686b6e98ac08f1
Sushmakg <sushmakgsushmakg4@gmail.com>
2025-03-10 16:08:12 +0530

    Create timeconversion.py

---
+++ timeconversion.py
@@ -0,0 +1,10 @@
+#coversion of minutes to hour
+min =int(input("enter time in minutes"))
+hour= min//60
+remainingminutes=min%60
+print(hour)
+print(remainingminutes)
+output
+enter time in minutes155
+2
+35

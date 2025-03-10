cebfc762018af1fe479ccc72ad250203ef109d46
Sushmakg <sushmakgsushmakg4@gmail.com>
2025-03-10 15:45:57 +0530

    Create SI.py

---
+++ SI.py
@@ -0,0 +1,12 @@
+#simple interest
+p=int(input("enter the value of p"))
+n=int(input("enter the value of n"))
+r=float(input("enter the value of r"))
+si=(p*n*r)/100
+print(si)
+
+output:
+enter the value of p1200
+enter the value of n2
+enter the value of r1.4
+33.6

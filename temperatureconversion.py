c82571d1c8a247bc6603686fad057bd37492decc
Sushmakg <sushmakgsushmakg4@gmail.com>
2025-03-10 15:23:14 +0530

    Update temperatureconversion.py

--- temperatureconversion.py
+++ temperatureconversion.py
@@ -6,3 +6,14 @@ print(farenheit)
 output:
 enter the value in celsius33
 91.4
+
+f to c
+
+#temperature conversion 
+farenheit=float(input("enter the value in celsius"))
+celsius=5/9*(farenheit-32)
+print(celsius)
+
+output :
+enter the value in celsius20
+-6.666666666666667

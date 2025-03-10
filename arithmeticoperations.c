627e34a4b549c05a0215f1ed9eddf14e35d252a3
Sushmakg <sushmakgsushmakg4@gmail.com>
2025-03-10 14:54:13 +0530

    Create arithmeticoperations.c

---
+++ arithmeticoperations.c
@@ -0,0 +1,10 @@
+#arithmetic operations 
+a= int(input("enter a"))
+b= int(input("enter b"))
+print("addition is",a+b,"subtraction is",a-b,"multiplication is",a*b,"division is",a/b)
+
+  
+output
+  enter a10
+enter b5
+addition is 15 subtraction is 5 multiplication is 50 division is 2.0

c11604c93445f462b9098fa68ae44937edca8427
Sushmakg <sushmakgsushmakg4@gmail.com>
2025-03-10 15:57:36 +0530

    Create areraandperimeterofrect.py

---
+++ areraandperimeterofrect.py
@@ -0,0 +1,10 @@
+l=int(input("enter length"))
+w=int(input("enter width"))
+area=l*w
+perimeter=2*(l+w)
+print(area, "\nperimerter is", perimeter)
+output
+enter length2
+enter width2
+4 
+perimerter is 8

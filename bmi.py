e9821f5c301887b02b6459a4de6c733cd70b408e
Sushmakg <sushmakgsushmakg4@gmail.com>
2025-03-10 15:33:41 +0530

    Update bmi.py

--- bmi.py
+++ bmi.py
@@ -1,9 +1,10 @@
+
 a=float(input("enter height"))
 b=int(input("enter your weight"))
 bmi=b/(a*a)
 print(bmi)
 
-output 
-enter height148
+output:
+enter height1.48
 enter your weight54
-0.0024653031409788166
+24.653031409788166

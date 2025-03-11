56bb63d108c958bb9381d7c5e6ad52cb4f462dab
Sushmakg <sushmakgsushmakg4@gmail.com>
2025-03-11 10:38:45 +0530

    Create sumofrangeofnumbers.py

---
+++ sumofrangeofnumbers.py
@@ -0,0 +1,15 @@
+num1=int(input("enter start range"))
+num2=int(input("enter end range"))
+sum=0
+for i in range(num1,num2+1):
+  sum=sum+i
+  print(sum) 
+
+enter start range1
+enter end range6
+1
+3
+6
+10
+15
+21

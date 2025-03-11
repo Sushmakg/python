d53445f12b00765a2c9c929e8ba705d729fe2411
Sushmakg <sushmakgsushmakg4@gmail.com>
2025-03-11 10:26:29 +0530

    Create oddoreven.py

---
+++ oddoreven.py
@@ -0,0 +1,9 @@
+#odd or even 
+a=int(input("enter a number"))
+if a %2==0:
+  print("the entered number is even")
+else:
+  print("the entered number is odd") 
+output 
+enter a number5
+the entered number is odd
